4e89aa785c9502c3d083ee6e8e060a422e80c041
Sammish Thundiyil <sammishthundiyil@Sammishs-MacBook-Pro-2.local>
2023-11-16 20:05:49 +0530

    update

---
+++ export_fixtures.py
@@ -0,0 +1,16 @@
+fixtures = [
+    {
+        "doctype": "Custom Field",
+        "filters": [
+            [
+                "name",
+                "in",
+                [
+                    "Payment Entry-sales_person",
+                ]
+            ]
+        ]
+    }
+]
+
+# bench --site site_name export-fixtures
