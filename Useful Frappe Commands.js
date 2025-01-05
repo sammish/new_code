ff879e8cbff29355111be7ea2dcd720d369bcd5e
Sammish Thundiyil <sammish.thundiyil@gmail.com>
2025-01-05 15:41:52 +0530

    Update Useful Frappe Commands.js

--- Useful Frappe Commands.js
+++ Useful Frappe Commands.js
@@ -5,6 +5,8 @@ bench migrate
 bench restart
 bench update --reset && bench setup requirements && bench build && bench restart && bench setup requirements --python && bench build && bench setup requirements && bench migrate && python3.10 -m pip install --upgrade pip && pip3 install --upgrade frappe-bench 
 
+{{ frappe.db.get_value("Customer", doc.customer, "tax_id") }}
+
 bench --site site_name scheduler resume
 
 letterhead
