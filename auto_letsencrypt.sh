6f85eaca39425705eebbfa7809994832cc3f40ad
Sammish Thundiyil <sammish.thundiyil@gmail.com>
2025-02-16 11:26:13 +0530

    Update auto_letsencrypt.sh

--- auto_letsencrypt.sh
+++ auto_letsencrypt.sh
@@ -2,6 +2,7 @@ sudo crontab -e
 59 23 28-31 * * [ "$(date -d tomorrow +\%d)" == "01" ] && systemctl stop nginx && certbot renew && systemctl start nginx
 
 
-sudo systemctl stop nginx
-sudo certbot renew
-sudo systemctl start nginx
+sudo systemctl stop nginx && sudo certbot renew && sudo systemctl start nginx
+
+
+
